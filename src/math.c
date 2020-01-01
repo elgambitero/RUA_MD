9d5336901ffaf4636151661c510c7c316a5ef460
Jaime García Villena <garciavillena.jaime@gmail.com>
2020-01-01 20:27:19 +0100

    Add Rect type and an intersect function

--- src/math.c
+++ src/math.c
@@ -1,5 +1,18 @@
 #include "math.h"
 
+Rect intersect(Rect a, Rect b){
+  Rect c;
+  c.pos[X] = a.pos[X] < b.pos[X] ? b.pos[X] : a.pos[X];
+  c.pos[Y] = a.pos[Y] < b.pos[Y] ? b.pos[Y] : a.pos[Y];
+  c.size[X] = c.pos[X] == a.pos[X] ? 
+    a.pos[X] + a.size[X] - c.pos[X] :
+    b.pos[X] + b.size[X] - c.pos[X];
+  c.size[Y] = c.pos[Y] == a.pos[Y] ? 
+    a.pos[Y] + a.size[Y] - c.pos[Y] :
+    b.pos[Y] + b.size[Y] - c.pos[Y];
+  return c;
+}
+
 u16 saturate(u16 num, u16 min, u16 max){
   return num > max ? max :
          num < min ? min : num;
--- src/math.h
+++ src/math.h
@@ -3,6 +3,16 @@
 
 #include <genesis.h>
 
+#define X 0
+#define Y 1
+
+typedef struct Rect{
+    u32 pos[2];
+    u16 size[2];
+} Rect;
+
+Rect intersect(Rect a, Rect b);
+
 u16 saturate(u16 num, u16 min, u16 max);
 
 #endif
